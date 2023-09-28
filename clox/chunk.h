df661c1f1692b29bbfcaf4cd13c43afc5a331be0
Natniif <fintan.hardy@gmail.com>
2023-09-28 16:24:53 +0100

    compiling expressions chapter finished

--- clox/chunk.h
+++ clox/chunk.h
@@ -7,6 +7,12 @@
 typedef enum {
     OP_CONSTANT,
     OP_NEGATE,
+
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+
     OP_RETURN,
 } OpCode;
 
--- clox/compiler.c
+++ clox/compiler.c
@@ -28,6 +28,14 @@ typedef enum {
     PREC_PRIMARY
 } Precedence;
 
+// function pointer 
+typedef void (*ParseFn)();
+
+typedef struct {
+    ParseFn prefix; 
+    ParseFn infix; 
+    Precedence precedence;
+} ParseRule;
 
 Parser parser;
 
@@ -116,6 +124,32 @@ static uint8_t makeConstant(Value value) {
 
 static void endCompiler() {
     emitReturn();
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+    if (!parser.hadError) {
+        disassembleChunk(currentChunk(), "code");
+    }
+#endif
+}
+
+static void expression(); 
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence Precedence);
+
+static void binary() {
+    // left operand has already been consumed
+    TokenType operatorType = parser.previous.type;
+    ParseRule* rule = getRule(operatorType);
+    // cast 
+    parserPrecedence((Precedence)(rule->precedence+1));
+
+    switch (operatorType) {
+        case TOKEN_PLUS:          emitByte(OP_ADD); break;
+        case TOKEN_MINUS:         emitByte(OP_SUBTRACT); break;
+        case TOKEN_STAR:          emitByte(OP_MULTIPLY); break;
+        case TOKEN_SLASH:         emitByte(OP_DIVIDE); break;
+        default: return; // Unreachable.
+    }
 }
 
 // dealing with parenthesis
@@ -142,12 +176,78 @@ static void unary() {
         case TOKEN_MINUS: emitByte(OP_NEGATE); break;
         default: return;
     }
-
 }
+
+ParseRule rules[] = {
+  [TOKEN_LEFT_PAREN]    = {grouping, NULL,   PREC_NONE},
+  [TOKEN_RIGHT_PAREN]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LEFT_BRACE]    = {NULL,     NULL,   PREC_NONE}, 
+  [TOKEN_RIGHT_BRACE]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_COMMA]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_DOT]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_MINUS]         = {unary,    binary, PREC_TERM},
+  [TOKEN_PLUS]          = {NULL,     binary, PREC_TERM},
+  [TOKEN_SEMICOLON]     = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SLASH]         = {NULL,     binary, PREC_FACTOR},
+  [TOKEN_STAR]          = {NULL,     binary, PREC_FACTOR},
+  [TOKEN_BANG]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_BANG_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EQUAL]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EQUAL_EQUAL]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_GREATER]       = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_GREATER_EQUAL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LESS]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LESS_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_STRING]        = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
+  [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FALSE]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FOR]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FUN]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_IF]            = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_NIL]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_OR]            = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_TRUE]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_ERROR]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EOF]           = {NULL,     NULL,   PREC_NONE},
+};
+
 
 // starts at current token and parses any expression at the given precedence level or higher
 static void parsePrecedence(Precedence precedence) {
+    advance();
+    // read the current token and look up the corresponding ParseRule
+    ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+    // if no prefix parser then token must be syntax error
+    if (prefixRule == NULL) {
+        error("Expect expression.");
+        return;
+    }
 
+    prefixRule();
+
+    // while precedence of operation of following token is lower advance and repeat
+    while (precedence <= getRule(parser.current.type)->precedence) {
+        advance(); 
+        // if following token not infix then ends 
+        ParseFn infixRule = getRule(parser.previous.type)->infix;
+        // e.g. if following token is '-' then infixRule() = binary()
+        infixRule();
+    }
+}
+
+// returns rule at given index
+static ParseRule* getRule(TokenType type) {
+    return &rules[type];
 }
 
 static void expression() {
